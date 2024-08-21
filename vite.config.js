3cadd2fd1ffafc654995aeed9438f26a452dca53
Rathod Jaimin <125123512+jaimin123-r@users.noreply.github.com>
2024-08-21 14:49:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+// vite.config.js
+
+export default {
+  build: {
+    rollupOptions: {
+      external: ["C:/Users/jaimi/Desktop/RJ/PORTFOLIO/public/portfo_img.jpg"]
+    }
+  }
+}
