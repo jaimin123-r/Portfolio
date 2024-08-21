def1af6d887c1f325241c62ff9f9c60536300a22
Rathod Jaimin <125123512+jaimin123-r@users.noreply.github.com>
2024-08-21 15:08:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    rollupOptions: {
-      external: ["https://drive.google.com/file/d/1fCJXd753WYnLFS3p1jK8eqTJjJ56RVpv/view?usp=drive_link"]
-    }
-  }
 })
