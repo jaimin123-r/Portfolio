1691cf38e9c92dcddf3830c1f8a631242c876e5b
Rathod Jaimin <125123512+jaimin123-r@users.noreply.github.com>
2024-08-21 15:02:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
    build: {
     rollupOptions: {
-      external: ["C:/Users/jaimi/Desktop/RJ/PORTFOLIO/public/portfo_img.jpg"]
+      external: ["https://drive.google.com/file/d/1fCJXd753WYnLFS3p1jK8eqTJjJ56RVpv/view?usp=drive_link"]
     }
   }
 })
